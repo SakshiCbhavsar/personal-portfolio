1093b3102af501c8c2ed721afbf9bceec52743c5
unknown <sakshibhavsar2018@gmail.com>
2024-07-16 11:59:01 +0530

    Fix ESLint errors

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
@@ -11,8 +11,9 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer", "Full Stack Developer"];
   const period = 2000;
+
+  const toRotate = useMemo(() => ["Web Developer", "Web Designer", "UI/UX Designer", "Full Stack Developer"], []);
 
   const tick = useCallback(() => {
     let i = loopNum % toRotate.length;
@@ -52,7 +53,7 @@ export const Banner = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Welcome to my Portfolio</span>
-                  <h1>{`Hi! I'm Sakshi`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer", "Full Stack Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <h1>{`Hi! I'm Sakshi`} <span className="txt-rotate" dataPeriod="1000" data-rotate={JSON.stringify(toRotate)}><span className="wrap">{text}</span></span></h1>
                   <p>A dedicated and skilled software professional with a strong background in building and maintaining web applications and Software testing. Seeking to leverage my expertise in front-end and back-end technologies to contribute to innovative projects and drive business success.</p>
                     <div className="button-wrapper">
                       <Link to="connect" smooth={true} duration={500}>
